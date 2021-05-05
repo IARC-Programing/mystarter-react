a17401ec53d4d761fc5237493d76607cf26043ea
Theethawat Savastham <5910110150@psu.ac.th>
2021-05-05 11:12:09 +0700

    Update Topbar

--- 02-tailwind-install/src/Components/Footer.js
+++ 02-tailwind-install/src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="text-center">
+    <div className="text-center p-2">
       <h3 className="text-sm">
         &copy; 2021 Intelligent Automation Research Center
       </h3>
--- 02-tailwind-install/src/Components/Topbar.js
+++ 02-tailwind-install/src/Components/Topbar.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Topbar() {
   return (
-    <div>
-      <h3 className="text-center">Monitoring System</h3>
+    <div className="bg-gray-100 p-4 flex justify-center">
+      <h3 className="text-2xl  text-green-300 text-center border-2 border-green-400">
+        Monitoring System
+      </h3>
     </div>
   )
 }
