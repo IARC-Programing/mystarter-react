a63eeba511c522b89821a8cfa0dda3d8c5da8a96
theethawat <5910110150@psu.ac.th>
2021-05-12 12:32:52 +0700

    PT-2 Example

--- 04-CallingAPIs/src/App.js
+++ 04-CallingAPIs/src/App.js
@@ -4,13 +4,36 @@ import React, { useState, useEffect } from 'react'
 
 import Topbar from './Components/Topbar'
 import Footer from './Components/Footer'
+import ControlCard from './Components/ServerControl/ControlCard'
+
+const serverInfomation = [
+  {
+    name: 'ระบบทดสอบ Logistic',
+    url: 'https://test-logistic.eappsoft.net',
+    controlURL: 'https://.....ngrok.io/manage'
+  },
+  {
+    name: 'บริษัท สินเอเชี่ยน อิมพอร์ต เอ็กซ์พอร์ต จำกัด',
+    url: 'https://sinasian.eappsoft.net',
+    controlURL: 'https://.....ngrok.io/manage'
+  },
+  {
+    name: 'บริษัท ได้ดีเอ็กเพรส จำกัด',
+    url: 'https://daideeexpress.eappsoft.net',
+    controlURL: 'https://.....ngrok.io/manage'
+  }
+]
 
 function App () {
   return (
     <div>
       <Topbar />
 
-      <div className='container min-h-screen'></div>
+      <div className='container min-h-screen'>
+        {serverInfomation.map((eachInformation, index) => (
+          <ControlCard key={index} serverData={eachInformation} />
+        ))}
+      </div>
 
       <Footer />
     </div>
--- 04-CallingAPIs/src/Components/ServerControl/ControlCard.js
+++ 04-CallingAPIs/src/Components/ServerControl/ControlCard.js
@@ -2,12 +2,14 @@ import React from 'react'
 import axios from 'axios'
 
 export default function ControlCard ({ serverData }) {
-  const url = serverData.url
+  const url = serverData.controlURL
 
   const handleStart = () => {
     axios.post(url + '/docker/start').then(res => {
       if (res.status === 200) {
         alert('ทำการเปิดสำเร็จ')
+      } else {
+        alert('ไม่สามารถทำการ Start ได้')
       }
     })
   }
@@ -16,6 +18,8 @@ export default function ControlCard ({ serverData }) {
     axios.post(url + '/docker/restart').then(res => {
       if (res.status === 200) {
         alert('ทำการ Restart สำเร็จ')
+      } else {
+        alert('ไม่สามารถทำการ Restart ได้')
       }
     })
   }
@@ -24,16 +28,43 @@ export default function ControlCard ({ serverData }) {
     axios.post(url + '/docker/stop').then(res => {
       if (res.status === 200) {
         alert('ทำการปิดสำเร็จ')
+      } else {
+        alert('ไม่สามารถทำการปิดได้')
       }
     })
   }
   return (
-    <div className='border m-1 rounded-md '>
-      <h2>โปรแกรม {serverData.name} </h2>
-      <h3>URL {serverData.url}</h3>
-      <button className='rounded-md bg-green-500'> Start </button>
-      <button className='rounded-md bg-yellow-500'> Restart </button>
-      <button className='rounded-md bg-red-500'> Stop </button>
+    <div className='border m-1 rounded-md p-2'>
+      <div className='flex'>
+        <div className='w-1/2'>
+          <h2>โปรแกรม {serverData.name} </h2>
+          <h3>URL {serverData.url}</h3>
+          <h3>Control URL {serverData.controlURL}</h3>
+        </div>
+        <div className='w-1/2 flex'>
+          <button
+            className='rounded-md bg-green-500'
+            onClick={() => handleStart()}
+          >
+            {' '}
+            Start{' '}
+          </button>
+          <button
+            className='rounded-md bg-yellow-500'
+            onClick={() => handleRestart()}
+          >
+            {' '}
+            Restart{' '}
+          </button>
+          <button
+            className='rounded-md bg-red-500'
+            onClick={() => handleStop()}
+          >
+            {' '}
+            Stop{' '}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
